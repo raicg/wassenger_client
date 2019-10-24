7494d0b6f8b9204534bfa95ab9f77e2e1ee31cc7
Raí Gondim <raicg2@gmail.com>
2019-10-24 20:25:22 -0300

    changing to OpenStruct

--- app/models/wassenger_client/event.rb
+++ app/models/wassenger_client/event.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 module WassengerClient
-  class Event < WassengerClient::Client
-    def message
-      WassengerClient::Message.new(data)
-    end
+  class Event < OpenStruct
+    class << self
 
-    def sent_message?
-      event == 'message:out:sent'
-    end
+      def message
+        WassengerClient::Message.new(data)
+      end
 
-    def failed_message?
-      event == 'message:out:failed'
-    end
+      def sent_message?
+        event == 'message:out:sent'
+      end
 
-    def in_new?
-      event == 'message:in:new'
-    end
+      def failed_message?
+        event == 'message:out:failed'
+      end
+
+      def in_new?
+        event == 'message:in:new'
+      end
   end
 end
