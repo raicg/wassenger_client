e70036db557aae92dc1961455a86bf37b1300bbd
Raí Gondim <raicg2@gmail.com>
2019-10-25 16:34:14 -0300

    updating file

--- app/models/wassenger_client/file.rb
+++ app/models/wassenger_client/file.rb
@@ -1,14 +1,38 @@
 # frozen_string_literal: true
 
 module WassengerClient
-  class File < WassengerClient::Client
+  class File < OpenStruct
     class << self
       def base_url
         WassengerClient::Client.base_url + 'files'
       end
 
-      def upload(params = {})
-        post(params)
+      def get(query = {})
+        WassengerClient::Client.get(base_url, '', {}, '', query)
+      end
+
+      def upload(params = {}, query = {})
+        WassengerClient::Client.post(base_url, '', params, '', query)
+      end
+
+      def preview_image(id = '0')
+        WassengerClient::Client.get(base_url, id, {}, '/preview')
+      end
+
+      def download(id = '0')
+        WassengerClient::Client.get(base_url, id, {}, '/download')
+      end
+
+      def get_details(id = '0')
+        WassengerClient::Client.get(base_url, id)
+      end
+
+      def update(id = '0', params = {})
+        WassengerClient::Client.patch(base_url, id, params)
+      end
+
+      def delete(id = '0')
+        WassengerClient::Client.delete(base_url, id)
       end
     end
   end
