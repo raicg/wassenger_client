f24096e476728a82c293057f60754c4ccbe4cd9a
Raí Gondim <raicg2@gmail.com>
2019-10-25 16:33:00 -0300

    file endpoint spec

---
+++ spec/models/wassenger_client/file_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'json'
+
+RSpec.describe WassengerClient::File, type: :model do
+  it 'it should connect with the api endpoint' do
+    response = Net::HTTP.get(URI.parse(WassengerClient::File.base_url))
+    expect(JSON.parse(response)['status']).to be_in([200, 403])
+  end
+end
